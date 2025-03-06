133b4fcc724df0f7334f3b544c1ccd194c1db16a
omerguzelelectronicguy <omer.guzel@agu.edu.tr>
2025-03-06 10:53:47 +0300

    redundant codes are deleted.

--- src/user_apps.c
+++ src/user_apps.c
@@ -42,20 +42,4 @@ void user_app2(void) {
         // Yield to other tasks
         syscall(SYS_YIELD, 0);
     }
-}/* main.c - Entry point for the firmware */
-#include "firmware.h"
-#include "supervisor.h"
-#include "user_apps.h"
-
-int main(void) {
-    // Initialize the firmware in machine mode
-    firmware_init();
-    
-    // Switch to supervisor mode
-    switch_to_supervisor();
-    
-    // We should never reach here as control is transferred
-    while(1) { /* Should never reach here */ }
-    
-    return 0;
 }
